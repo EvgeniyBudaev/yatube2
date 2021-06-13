5faba6ea79f214bf4fd8ce759615008cf6694ae7
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-13 12:59:17 +0300

    Профайл пользователя и страница записи

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 from yatube2.settings import POSTS_IN_PAGINATOR
 from .models import Post, Group
+
+User = get_user_model()
 
 
 def index(request):
@@ -12,10 +15,10 @@ def index(request):
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     return render(
-         request,
-         'posts/index.html',
-         {'page': page}
-     )
+        request,
+        'posts/index.html',
+        {'page': page}
+    )
 
 
 def group_posts(request, slug):
@@ -31,3 +34,33 @@ def group_posts(request, slug):
     }
 
     return render(request, "posts/group.html", context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all().order_by("-pub_date")
+    paginator = Paginator(posts, POSTS_IN_PAGINATOR)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    context = {
+        'page': page,
+        'author': author,
+        "posts_count": author.posts.count()
+    }
+
+    return render(request, 'posts/profile.html', context)
+
+
+def post_view(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+    author = post.author
+    posts_count = author.posts.count()
+
+    context = {
+        "post": post,
+        "author": author,
+        "posts_count": posts_count,
+    }
+
+    return render(request, 'posts/post.html', context)
