8f34cf7d880b9eff0f47d099ee9c2ef221ec6d00
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-06-13 09:48:43 +0300

    Paginator

--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
+from yatube2.settings import POSTS_IN_PAGINATOR
 from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "posts/index.html", {"posts": latest})
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(
+         request,
+         'posts/index.html',
+         {'page': page}
+     )
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.filter(group=group).order_by("-pub_date")[:12]
-    # posts = group.posts.all()
+    posts = group.posts.filter(group=group).order_by("-pub_date")
+    paginator = Paginator(posts, POSTS_IN_PAGINATOR)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
 
     context = {
         "group": group,
-        "page": posts
+        "page": page
     }
 
     return render(request, "posts/group.html", context)
